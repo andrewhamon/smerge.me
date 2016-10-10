bcb4a61246f05ae517eb58573b7859a4cbadef90
Andrew Hamon <andrew@hamon.cc>
2016-10-10 01:18:27 -0400

    Add github repo name to views

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,9 @@ class Link < ApplicationRecord
   belongs_to :spotify_user
 
   validates :spotify_playlist_id, uniqueness: true
+
+  def github_repo_name
+    client = Octokit::Client.new(access_token: github_user.access_token)
+    client.repository(github_repo_id)["full_name"]
+  end
 end
