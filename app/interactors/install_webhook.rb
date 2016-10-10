1ff2992f2ba01bc34adf7df5f157811612ac69ff
Andrew Hamon <andrew@hamon.cc>
2016-10-09 21:54:33 -0400

    Add link_id to webhook url

--- app/interactors/install_webhook.rb
+++ app/interactors/install_webhook.rb
@@ -10,12 +10,18 @@ class InstallWebhook
 
   def call
     client = Octokit::Client.new(access_token: github_user.access_token)
-    webhook_config = { url: WEBHOOK_URL, content_type: "json" }
+    webhook_config = { url: webhook_url, content_type: "json" }
     webhook_params = { events: ["push"], active: true }
     client.create_hook(link.github_repo_id, "web", webhook_config, webhook_params)
   end
 
   def self.call(link:)
     new(link: link).call
+  end
+
+  private
+
+  def webhook_url
+    WEBHOOK_URL + "?link_id=#{link.id}"
   end
 end
